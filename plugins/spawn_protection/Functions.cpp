c3f020be7ca4c576fa2f6bdccb02178e43d1dd1b
Coryf88 <Coryf88@users.noreply.github.com>
2017-08-28 09:10:49 -0500

    Rewrote Spawn Protection Plugin

--- plugins/spawn_protection/Functions.cpp
+++ plugins/spawn_protection/Functions.cpp
@@ -1,7 +1,12 @@
 class BRM_FMK_SpawnProtection {
 	class init {
 		file = "\broma_framework\plugins\spawn_protection\functions";
-		class setParams {};	
+		class setParams {};
 		class postinit {};
 	};
-}; 
+	class functions {
+		file = "\broma_framework\plugins\spawn_protection\functions";
+		class allowDamage {};
+		class clientMarker {};
+	};
+};
