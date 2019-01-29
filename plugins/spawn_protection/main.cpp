083b19c326487d19a50946ab963836703237a6a2
Neefay <notnife@hotmail.com>
2019-01-29 14:32:46 -0200

    Update main.cpp

--- plugins/spawn_protection/main.cpp
+++ plugins/spawn_protection/main.cpp
@@ -2,7 +2,7 @@
 	class spawn_protection {
 		name = "Spawn protection";
 		version = 1;
-		authors[] = {"Nife"};
+		authors[] = {"Nife", "Coryf88"};
 		description = "Creates a protection zone around the insertion point, which can last forever or as long as the mission parameters determine, ensuring neither players or friendly vehicles can take damage while inside.";
 	};
 #endif
