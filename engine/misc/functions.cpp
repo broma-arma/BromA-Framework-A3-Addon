c310b2bc18bcfe5fd919787246abe87309a28ecc
Coryf88 <Coryf88@users.noreply.github.com>
2020-10-14 09:04:28 -0500

    Remove debug code

--- engine/misc/functions.cpp
+++ engine/misc/functions.cpp
@@ -1,7 +1,7 @@
 class misc {
 	file = "\broma_framework\engine\misc";
-	class getSpawnPoint {recompile = 1;};
-	class stampTime{recompile = 1;};
-	class syncTime{recompile = 1;};
-	class getMusic {recompile = 1;};
+	class getSpawnPoint {};
+	class stampTime{};
+	class syncTime{};
+	class getMusic {};
 };
