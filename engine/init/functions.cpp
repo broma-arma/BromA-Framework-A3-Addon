acb016f9575351bd22046aee1c13281498667549
Coryf88 <Coryf88@users.noreply.github.com>
2022-07-24 17:25:48 -0500

    Move fn_assignSideProperties.sqf and fn_loadBriefing.sqf to engine/mission

--- engine/init/functions.cpp
+++ engine/init/functions.cpp
@@ -2,7 +2,5 @@ class init {
 	file = "\broma_framework\engine\init";
 	class engine_pre {preInit = 1;};
 	class initVariables {};
-	class assignSideProperties {};
 	class engine_post {postInit = 1;};
-	class loadBriefing {};
 };
--- engine/mission/functions.cpp
+++ engine/mission/functions.cpp
@@ -1,5 +1,7 @@
 class mission {
 	file = "\broma_framework\engine\mission";
 	class loadMissionSettings {};
+	class assignSideProperties {};
+	class loadBriefing {};
 	class playableMissionEntities {};
 };
