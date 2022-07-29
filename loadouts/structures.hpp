2cae3883409a7be96d81a547bc13e101a945b36b
Coryf88 <Coryf88@users.noreply.github.com>
2022-07-28 23:47:55 -0500

    Simplify structure IDs

--- loadouts/structures.hpp
+++ loadouts/structures.hpp
@@ -1,6 +1,6 @@
-HIGH_TIER = "\broma_framework\loadouts\factions\structures\high_tier_army.sqf";
-MID_TIER = "\broma_framework\loadouts\factions\structures\mid_tier_army.sqf";
-LOW_TIER = "\broma_framework\loadouts\factions\structures\low_tier_army.sqf";
-FRANCE_STRUCTURE = "\broma_framework\loadouts\factions\structures\france_structure.sqf";
-US_STRUCTURE = "\broma_framework\loadouts\factions\structures\us_structure.sqf";
+HIGH = "\broma_framework\loadouts\factions\structures\high_tier_army.sqf";
+MID = "\broma_framework\loadouts\factions\structures\mid_tier_army.sqf";
+LOW = "\broma_framework\loadouts\factions\structures\low_tier_army.sqf";
+FRANCE = "\broma_framework\loadouts\factions\structures\france_structure.sqf";
+US = "\broma_framework\loadouts\factions\structures\us_structure.sqf";
 DEFAULT = "\broma_framework\loadouts\factions\structures\high_tier_army.sqf";
