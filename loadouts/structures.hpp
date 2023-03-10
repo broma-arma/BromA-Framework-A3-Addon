2350048380369f677172c7dbb91df49155cfc873
Coryf88 <Coryf88@users.noreply.github.com>
2023-03-10 12:56:33 -0600

    Fix factions and structures

--- loadouts/structures.hpp
+++ loadouts/structures.hpp
@@ -1,6 +1,6 @@
-HIGH = "\broma_framework\loadouts\factions\structures\high_tier_army.sqf";
-MID = "\broma_framework\loadouts\factions\structures\mid_tier_army.sqf";
-LOW = "\broma_framework\loadouts\factions\structures\low_tier_army.sqf";
-FRANCE = "\broma_framework\loadouts\factions\structures\france_structure.sqf";
-US = "\broma_framework\loadouts\factions\structures\us_structure.sqf";
-DEFAULT = "\broma_framework\loadouts\factions\structures\high_tier_army.sqf";
+HIGH = "\broma_framework\loadouts\oldFactions\structures\high_tier_army.sqf";
+MID = "\broma_framework\loadouts\oldFactions\structures\mid_tier_army.sqf";
+LOW = "\broma_framework\loadouts\oldFactions\structures\low_tier_army.sqf";
+FRANCE = "\broma_framework\loadouts\oldFactions\structures\france_structure.sqf";
+US = "\broma_framework\loadouts\oldFactions\structures\us_structure.sqf";
+DEFAULT = "\broma_framework\loadouts\oldFactions\structures\high_tier_army.sqf";
