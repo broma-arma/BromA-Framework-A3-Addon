d77a5f04b6d7a33b615c12979528b15936ac8ba8
Coryf88 <Coryf88@users.noreply.github.com>
2024-09-01 09:58:03 -0500

    Categorize API functions

--- engine/api/functions.cpp
+++ engine/api/functions.cpp
@@ -1,27 +1,67 @@
 class api {
 	file = "\broma_framework\engine\api";
-	class getSideInfo {};
-	class newMarker{};
-	class newMarkerArea{};
-	class newMarkerIcon{};
-	class weaponAway{};
-	class garrisonUnits{};
-	class showText{};
-	class colorToHex{};
-	class verboseArray{};
-	class unitFromName{};
-	class callCodeArea{};
-	class playGlobal{};
-	class getUnitsArray{};
-	class getFactionVehicles{};
-	class appendIndices{};
 	class reachTarget{};
-	class checkCasualties{};
-	class playCfgSound{};
 	class artillery{};
 	class cas{};
 	class halo{};
 	class paratroop{};
+};
+
+class api_ai {
+	file = "\broma_framework\engine\api\ai";
+	class garrisonUnits{};
+};
+
+class api_arrays {
+	file = "\broma_framework\engine\api\arrays";
+	class verboseArray{};
+	class appendIndices{};
+};
+
+class api_deprecated {
+	file = "\broma_framework\engine\api\deprecated";
+	class getUnitsArray{};
+	class getFactionVehicles{};
+};
+
+class api_markers {
+	file = "\broma_framework\engine\api\markers";
+	class newMarker{};
+	class newMarkerArea{};
+	class newMarkerIcon{};
+};
+
+class api_sides {
+	file = "\broma_framework\engine\api\sides";
+	class getSideInfo {};
+	class checkCasualties{};
+};
+
+class api_sounds {
+	file = "\broma_framework\engine\api\sounds";
+	class playGlobal{};
+	class playCfgSound{};
+};
+
+class api_triggers {
+	file = "\broma_framework\engine\api\triggers";
+	class callCodeArea{};
+};
+
+class api_ui {
+	file = "\broma_framework\engine\api\ui";
+	class showText{};
+	class colorToHex{};
+};
+
+class api_unit {
+	file = "\broma_framework\engine\api\unit";
+	class unitFromName{};
+};
+
+class api_weapons {
+	file = "\broma_framework\engine\api\weapons";
+	class weaponAway{};
 	class isDisposableLauncher{};
 };
 
