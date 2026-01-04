f0b9f81ed89aa2554fd4e619e72e5b6259d3214b
Coryf88 <Coryf88@users.noreply.github.com>
2026-01-04 09:24:38 -0600

    Add BromA insignia

--- CfgUnitInsignia.hpp
+++ CfgUnitInsignia.hpp
@@ -1,8 +1,12 @@
 class CfgUnitInsignia {
-	class BRM_FMK_Medic {
-		displayName = "Medic";
+	class BRM_FMK_BromA {
+		displayName = "BromA";
 		author = "BromA";
-		texture = "\broma_framework\assets\insignias\medic.paa";
+		texture = "\broma_framework\assets\images\group-logo.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+	};
+	class BRM_FMK_Medic: BRM_FMK_BromA {
+		displayName = "Medic";
+		texture = "\broma_framework\assets\insignias\medic.paa";
 	};
 };
